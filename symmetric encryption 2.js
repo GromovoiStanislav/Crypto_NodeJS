1bbb5475e246f8734ef468bbd4fdb3f3cf3455a3
Gromovoi Stanislav <gromila12@rambler.ru>
2023-08-14 10:40:46 +0600

    symmetric encryption 2

---
+++ symmetric encryption 2.js
@@ -0,0 +1,41 @@
+// симметричное шифрование
+const crypto = require('crypto');
+
+const secretKey = crypto.randomBytes(32).toString('hex');
+console.log('Secret Key:', secretKey);
+///////////////////////////////////////////////////////////////
+
+function encrypt(data, secretKey) {
+  const iv = crypto.randomBytes(16); // Инициализирующий вектор
+  const cipher = crypto.createCipheriv(
+    'aes-256-cbc',
+    Buffer.from(secretKey, 'hex'), // Преобразование в бинарные данные
+    iv
+  );
+  let encryptedData = cipher.update(JSON.stringify(data), 'utf-8', 'hex');
+  encryptedData += cipher.final('hex');
+  encryptedData = iv.toString('hex') + encryptedData;
+
+  return encryptedData;
+}
+
+const encryptedData = encrypt('Секретное сообщение', secretKey);
+console.log('Зашифрованное сообщение:', encryptedData);
+
+//////////////////////////////////////////////////////////////////////
+
+function decrypt(data, secretKey) {
+  const iv = Buffer.from(data.slice(0, 32), 'hex');
+  const encryptedData = data.slice(32);
+  const decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    Buffer.from(secretKey, 'hex'),
+    iv
+  );
+  let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+  decryptedData += decipher.final('utf-8');
+  return JSON.parse(decryptedData);
+}
+
+const decryptedData = decrypt(encryptedData, secretKey);
+console.log('Расшифрованное сообщение:', decryptedData);
